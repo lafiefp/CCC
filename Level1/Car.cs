607bbe2f2756b1d3398619bdab170725106b0d76
Franz <franz.profelt@gmail.com>
2015-11-06 14:18:03 +0100

    implemented car, changed .NET version

---
+++ Level1/Car.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Level1
+{
+    class Car
+    {
+        private int start;
+        public int Start
+        {
+            get { return this.start; }
+        }
+        private int end;
+        public int End
+        {
+            get { return this.start; }
+        }
+
+        private int elapsedTime;
+        public int ElapsedTime
+        {
+            get { return this.elapsedTime; }
+        }
+
+
+        public Car(int startpoint, int endpoint)
+        {
+            this.start = startpoint;
+            this.end = endpoint;
+            this.elapsedTime = 0;
+        }
+
+        public void Drive()
+        {
+            this.elapsedTime++;
+        }
+    }
+}
