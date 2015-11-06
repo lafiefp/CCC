f893f9b55fabc48a575af42970a411e46bdda081
Franz <franz.profelt@gmail.com>
2015-11-06 13:56:28 +0100

    implemented epic ArgumentParser

--- CCC/Program.cs
+++ CCC/Program.cs
@@ -4,6 +4,14 @@
     {
         static void Main(string[] args)
         {
+            var list = Utils.ArgumentParser.ParseFile(@"C:\Users\Franz\Documents\input-level2.txt");
+            foreach (var l in list)
+            {
+                foreach(var d in l)
+                    System.Console.WriteLine(string.Format("{0} - {1}", d.Key, d.Value));
+                System.Console.WriteLine("----");
+            }
+            System.Console.ReadLine();
         }
     }
 }
---
+++ CCC/Utils/ArgumentParser.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace CCC.Utils
+{
+    class ArgumentParser
+    {
+        public static List<Dictionary<string, object>> ParseFile(string filename)
+        {
+            var lines = File.ReadAllLines(filename);
+            var list = new List<Dictionary<string, object>>();
+
+            foreach(var line in lines)
+            {
+                var dict = new Dictionary<string,object>();
+
+                var args = line.Split(' ');
+                for (var i = 1; i < args.Length; i++)
+                {
+                    var parts = args[i].Split('=');
+                    if (parts.Length > 1)
+                        dict.Add(parts[0], parts[1]);
+                }
+
+                list.Add(dict);
+            }
+            return list;
+        }
+
+
+
+    }
+}
